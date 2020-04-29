6e97e0ce077aa76c48a07deebf708d1d6c79322f
JuanCamilo08 <richin12345@gmail.com>
2020-04-29 18:04:01 -0400

    create the other folder for the next project

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import '@babel/polyfill';
-import app from './startup/app';
-import logger from './startup/logging';
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  logger.info(`server listening on port ${port}`);
-});
--- src/middlewares/validateObjectId.js
+++
@@ -1,8 +0,0 @@
-import { Types } from 'mongoose';
-
-export default (req, res, next) => {
-  if (!Types.ObjectId.isValid(req.params.id))
-    return res.status(400).json('Invalid ID');
-
-  next();
-};
